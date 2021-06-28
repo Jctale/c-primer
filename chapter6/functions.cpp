2983d676a3ee06cb26dbbab689a439108039f598
Jctale <causeiloveu@gapp.nthu.edu.tw>
2021-06-28 18:59:14 +0800

    nb update

--- chapter6/functions.cpp
+++ chapter6/functions.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include "functions.h"
-
+/*p.268~p.294*/
 using namespace std;
 
 int main()
 {
     double num;
-    while (cin >> num)
-        cout << abs(num) << endl;
+    //while (cin >> num)
+    //    cout << abs(num) << endl;
+    int arr[5] = {1, 2, 3, 4, 5};
+    fun(arr);
 }
 double abs(double val)
 {
     if (val > 0)
         return val;
     return -val;
+}
+int fun(int (&arr)[5])
+{
+    for (auto e : arr)
+        cout << e << endl;
 }
--- chapter6/functions.h
+++ chapter6/functions.h
@@ -1,4 +1,5 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 double abs(double val);
+int fun(int (&arr)[5]);
 #endif
